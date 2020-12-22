6c6cf5241fba53493c1b6e1864a0b77b3acab8ad
Prince Raju <princeraju7777@gmail.com>
2020-12-22 08:09:28 +0530

    Deployed code

--- src/App.js
+++ src/App.js
@@ -28,9 +28,9 @@ function App() {
       '.scroll-down-icon',
       {
         scrollY: {
-          translateX: [
+          opacity: [
             ['0', '300'],
-            [0, '200']
+            [1, 0]
           ]
         }
       }
@@ -41,9 +41,9 @@ function App() {
       '#initialTextContent',
       {
         scrollY: {
-          translateX: [
+          opacity: [
             ['0', '600'],
-            [0, '1.5 * pageWidth']
+            [1, 0]
           ]
         }
       }
@@ -53,9 +53,9 @@ function App() {
       '#logoContent',
       {
         scrollY: {
-          translateX: [
+          opacity: [
             ['0', '600'],
-            [0, '-1.5 * pageWidth']
+            [1, 0]
           ]
         }
       }
@@ -219,11 +219,12 @@ function App() {
           <div className="top-spacing"/>
           <b>18 Jan 2021</b>
           <div className="top-spacing"/>
-          <b>Wedding</b><br/>St. Mary's JSC, Pothanicad | &nbsp;
+          Live steam link will be available here on the wedding day.
+          {/* <b>Wedding</b><br/>St. Mary's JSC, Pothanicad | &nbsp;
           <a href="https://goo.gl/maps/9hiXsr5hGahgPg7R7" target="_blank" rel="noreferrer">Maps Link</a>
           <div className="top-spacing"/>
           <b>Reception</b><br/> C9 Event Center | &nbsp;
-          <a href="https://www.google.com/maps/place/C9+Event+Center+(unit+Of+C9+Hospitality)/@10.0076591,76.6053719,19.05z/data=!4m5!3m4!1s0x3b07e7abe15ee543:0xbf6bf821efe82d4!8m2!3d10.0075897!4d76.6057824" target="_blank" rel="noreferrer">Maps Link</a>
+          <a href="https://www.google.com/maps/place/C9+Event+Center+(unit+Of+C9+Hospitality)/@10.0076591,76.6053719,19.05z/data=!4m5!3m4!1s0x3b07e7abe15ee543:0xbf6bf821efe82d4!8m2!3d10.0075897!4d76.6057824" target="_blank" rel="noreferrer">Maps Link</a> */}
         </div>
         {<Wave />}
         <img src={ourPhoto} alt="" id="ourPhotoMain"/>
